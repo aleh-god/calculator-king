358c34409e2bb76a788b81bf3ce2aca5e2f9ef51
aleh-god <aleh.god@gmail.com>
2022-11-07 12:16:43 +0300

    fix: String max length validator

--- app/src/main/java/by/godevelopment/kingcalculator/domain/partiesdomain/usecases/ValidatePartyNameUseCase.kt
+++ app/src/main/java/by/godevelopment/kingcalculator/domain/partiesdomain/usecases/ValidatePartyNameUseCase.kt
@@ -7,12 +7,19 @@ import javax.inject.Inject
 class ValidatePartyNameUseCase @Inject constructor() {
 
     private val MIN_PARTY_NAME_LENGTH = 3
+    private val MAX_PARTY_NAME_LENGTH = 20
 
     fun execute(partyName: String): ValidationResult {
         if (partyName.length < MIN_PARTY_NAME_LENGTH) {
             return ValidationResult(
                 successful = false,
                 errorMessage = R.string.message_error_validate_party_name_length
+            )
+        }
+        if (partyName.length > MAX_PARTY_NAME_LENGTH) {
+            return ValidationResult(
+                successful = false,
+                errorMessage = R.string.message_error_validate_party_name_high_length
             )
         }
         return ValidationResult(successful = true)
